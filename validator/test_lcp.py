58d0ebe71eeb13585d437eea1567c1c5831f289b
Pim van Pelt <pim@ipng.nl>
2022-03-21 08:53:40 +0000

    Complete test_qinx now that get_*_by_name() returns tuples

--- validator/test_lcp.py
+++ validator/test_lcp.py
@@ -16,11 +16,22 @@ class TestLCPMethods(unittest.TestCase):
         ## self.assertFalse(lcp.is_unique(self.cfg, "e1.1000"))
 
     def test_qinx(self):
-        qint_ifname, qinq_iface = interface.get_by_name(self.cfg, "GigabitEthernet1/0/1.201")
+        qinx_ifname, qinx_iface = interface.get_by_name(self.cfg, "GigabitEthernet1/0/1.201")
         mid_ifname, mid_iface = interface.get_qinx_parent_by_name(self.cfg, "GigabitEthernet1/0/1.201")
         parent_ifname, parent_iface = interface.get_parent_by_name(self.cfg, "GigabitEthernet1/0/1.201")
 
-        # TODO(pim) - complete once get_*_by_name() returns a dict
-        # print("qinq", qinq_iface)
-        # print("mid", mid_iface)
-        # print("parent", parent_iface)
+        self.assertEqual(qinx_ifname, "GigabitEthernet1/0/1.201")
+        self.assertEqual(mid_ifname, "GigabitEthernet1/0/1.200")
+        self.assertEqual(parent_ifname, "GigabitEthernet1/0/1")
+
+        qinx_ifname, qinx_iface = interface.get_by_name(self.cfg, "GigabitEthernet1/0/1.201")
+        mid_ifname, mid_iface = interface.get_qinx_parent_by_name(self.cfg, "GigabitEthernet1/0/1.201")
+        parent_ifname, parent_iface = interface.get_parent_by_name(self.cfg, "GigabitEthernet1/0/1.201")
+
+        self.assertEqual(qinx_ifname, "GigabitEthernet1/0/1.201")
+        self.assertEqual(mid_ifname, "GigabitEthernet1/0/1.200")
+        self.assertEqual(parent_ifname, "GigabitEthernet1/0/1")
+
+        ifname, iface = interface.get_qinx_parent_by_name(self.cfg, "GigabitEthernet1/0/1.100")
+        self.assertIsNone(ifname)
+        self.assertIsNone(iface)
