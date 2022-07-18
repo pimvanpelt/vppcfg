18260e3465763f5d7a8065b33fb52cd1a3a90cc4
Pim van Pelt <pim@ipng.nl>
2022-07-18 07:36:14 +0000

    Call the correct new location of vppcfg.py

--- vppcfg/intest/intest.sh
+++ vppcfg/intest/intest.sh
@@ -7,6 +7,9 @@
 ## here should not be used although they can be a source of config inspiration :)
 
 ## Run me:
+# ./intest.sh 2>&1 | tee intest.out
+
+## In strict mode, any residual diff will be a hard error
 # ./intest.sh -strict 2>&1 | tee intest.out
 
 rm -f "intest.exec"
@@ -16,28 +19,28 @@ STRICT=0
 
 for i in hippo[0-9]*.yaml; do
   echo "Clearing: Moving to hippo-empty.yaml"
-  ../vppcfg plan -s ../schema.yaml -c hippo-empty.yaml -o /tmp/vppcfg-exec-empty
+  ../vppcfg.py plan -s ../schema.yaml -c hippo-empty.yaml -o /tmp/vppcfg-exec-empty
   [ -s /tmp/vppcfg-exec-empty ] && {
 	cat /tmp/vppcfg-exec-empty >> intest.exec
       vppctl exec /tmp/vppcfg-exec-empty
   }
   for j in hippo[0-9]*.yaml; do
     echo " - Moving to $i .. "
-    ../vppcfg plan -s ../schema.yaml -c $i -o /tmp/vppcfg-exec_$i
+    ../vppcfg.py plan -s ../schema.yaml -c $i -o /tmp/vppcfg-exec_$i
     [ -s /tmp/vppcfg-exec_$i ] && {
 	cat /tmp/vppcfg-exec_$i >> intest.exec
 	vppctl exec /tmp/vppcfg-exec_$i
     }
 
     echo " - Moving from $i to $j"
-    ../vppcfg plan -s ../schema.yaml -c $j -o /tmp/vppcfg-exec_${i}_${j}
+    ../vppcfg.py plan -s ../schema.yaml -c $j -o /tmp/vppcfg-exec_${i}_${j}
     [ -s /tmp/vppcfg-exec_${i}_${j} ] && {
 	cat /tmp/vppcfg-exec_${i}_${j} >> intest.exec
 	vppctl exec /tmp/vppcfg-exec_${i}_${j}
     }
 
     echo " - Checking that from $j to $j is empty"
-    ../vppcfg plan -s ../schema.yaml -c $j -o /tmp/vppcfg-exec_${j}_${j}_null
+    ../vppcfg.py plan -s ../schema.yaml -c $j -o /tmp/vppcfg-exec_${j}_${j}_null
     [ -s /tmp/vppcfg-exec_${j}_${j}_null ] && {
       echo " - ERROR Transition is not empty"
       cat /tmp/vppcfg-exec_${j}_${j}_null
