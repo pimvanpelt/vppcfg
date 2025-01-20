6879fa85ddee1ad75c0fa1f24e73043cabe52493
Pim van Pelt <pim@ipng.ch>
2025-01-20 17:19:45 +0100

    Release 0.0.7 with sFlow support

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="vppcfg",
-    version="0.0.6",
+    version="0.0.7",
     install_requires=[
         "requests",
         'importlib-metadata; python_version >= "3.8"',
